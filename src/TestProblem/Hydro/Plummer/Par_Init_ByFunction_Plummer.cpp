af982c64c955661270626a93c362e98ace6ec7a8
John ZuHone <jzuhone@gmail.com>
2020-03-22 18:42:54 -0400

    Set particle type in all test problems which do so

--- src/TestProblem/Hydro/Plummer/Par_Init_ByFunction_Plummer.cpp
+++ src/TestProblem/Hydro/Plummer/Par_Init_ByFunction_Plummer.cpp
@@ -49,6 +49,7 @@ static void   RanVec_FixRadius( const double r, double RanVec[] );
 //                ParPosX/Y/Z   : Particle position array with the size of NPar_ThisRank
 //                ParVelX/Y/Z   : Particle velocity array with the size of NPar_ThisRank
 //                ParTime       : Particle time     array with the size of NPar_ThisRank
+//                ParType       : Particle type     array with the size of NPar_ThisRank
 //                AllAttribute  : Pointer array for all particle attributes
 //                                --> Dimension = [PAR_NATT_TOTAL][NPar_ThisRank]
 //                                --> Use the attribute indices defined in Field.h (e.g., Idx_ParCreTime)
@@ -59,7 +60,7 @@ static void   RanVec_FixRadius( const double r, double RanVec[] );
 void Par_Init_ByFunction_Plummer( const long NPar_ThisRank, const long NPar_AllRank,
                                   real *ParMass, real *ParPosX, real *ParPosY, real *ParPosZ,
                                   real *ParVelX, real *ParVelY, real *ParVelZ, real *ParTime,
-                                  real *AllAttribute[PAR_NATT_TOTAL] )
+                                  real *ParType, real *AllAttribute[PAR_NATT_TOTAL] )
 {
 
    if ( MPI_Rank == 0 )    Aux_Message( stdout, "%s ...\n", __FUNCTION__ );
@@ -182,9 +183,12 @@ void Par_Init_ByFunction_Plummer( const long NPar_ThisRank, const long NPar_AllR
    } // if ( MPI_Rank == 0 )
 
 
-// synchronize all particles to the physical time on the base level
-   for (long p=0; p<NPar_ThisRank; p++)   ParTime[p] = Time[0];
-
+// synchronize all particles to the physical time on the base level,
+// set generic particle type
+   for (long p=0; p<NPar_ThisRank; p++) {
+      ParTime[p] = Time[0];
+      ParType[p] = PTYPE_GENERIC_MASSIVE;
+   }
 
 // get the number of particles in each rank and set the corresponding offsets
    if ( NPar_AllRank > (long)__INT_MAX__ )
